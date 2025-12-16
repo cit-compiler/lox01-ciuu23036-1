365d06188619e1b6dada90e4bc5aa1d6b39e1791
ciuu23036 <ciuu23036@g.nihon-u.ac.jp>
2025-12-16 11:33:56 +0900

    updata Lox.java and Paser.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -4,13 +4,21 @@ import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
-public class Paser {
+public class Parser {
     private static class ParseError extends RuntimeException {}
     private final List<Token> tokens;
     private int current = 0;
 
     Parser(List<Token> tokens){
         this.tokens = tokens;
+    }
+
+    Expr parse(){
+        try{
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
     }
 
     private Expr expression(){
@@ -84,6 +92,8 @@ public class Paser {
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
+
+        throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types){
@@ -127,5 +137,27 @@ public class Paser {
     private ParseError error(Token token, String message){
         Lox.error(token, message);
         return new ParseError();
+    }
+
+    private void synchronize(){
+        advance();
+        
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                  return;
+            }
+          
+          advance();
+        }  
     }
 }
