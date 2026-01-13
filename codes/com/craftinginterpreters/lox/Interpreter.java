8b817f0678f79faa201569929f8d9a1ccebdee09
ciuu23036 <ciuu23036@g.nihon-u.ac.jp>
2026-01-13 12:26:47 +0900

    new java

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -6,6 +6,8 @@ import javax.management.RuntimeErrorException;
 
 public class Interpreter implements Expr.Visitor<Object> , Stmt.Visitor<Void> {
     
+    private Environment environment = new Environment();
+    
     //void interpret(Expr expression) { 
     //    try {
     //       Object value = evaluate(expression);
@@ -42,6 +44,11 @@ public class Interpreter implements Expr.Visitor<Object> , Stmt.Visitor<Void> {
                 return -(double)right;
         }
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token opetator, Object operand){
@@ -106,6 +113,24 @@ public class Interpreter implements Expr.Visitor<Object> , Stmt.Visitor<Void> {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+        
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 
     @Override
