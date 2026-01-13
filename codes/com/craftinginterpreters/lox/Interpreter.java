d65384863a77799efce2bf28d85194dce707bf24
ciuu23036 <ciuu23036@g.nihon-u.ac.jp>
2026-01-13 10:28:18 +0900

    add RuntimeError.java

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -4,6 +4,15 @@ import javax.management.RuntimeErrorException;
 
 public class Interpreter implements Expr.Visitor<Object> {
     
+    void interpret(Expr expression) { 
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr){
         return expr.value;
@@ -45,6 +54,20 @@ public class Interpreter implements Expr.Visitor<Object> {
         if(a == null) return false;
 
         return a.equals(b);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
 
     @Override
@@ -97,4 +120,5 @@ public class Interpreter implements Expr.Visitor<Object> {
         }
         return null;
     }
+
 }
